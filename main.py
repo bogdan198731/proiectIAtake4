ce9e8c270d3e171fc6e29a15a2ca382d6d278449
bogdan198731 <bogdan198731@gmail.com>
2023-01-31 14:39:15 +0200

    Ajustat programul

--- main.py
+++ main.py
@@ -1,45 +1,56 @@
 import random
-
 from neuron import *
 
 
-train_data = pd.read_csv('C:/Users/bogda/PycharmProjects/date/mnist_train.csv')
-test_data = pd.read_csv('C:/Users/bogda/PycharmProjects/date/mnist_test.csv')
+#functie main, citirea fisierelor CSV, crearea unui obiect de tip retea si testarea rezultatelor
 
-train_data = np.array(train_data)
-dev_data = np.array(test_data)
-m, n = train_data.shape
+def main():
 
-t, l = dev_data.shape
+    m, X_train, Y_train, X_dev, Y_dev = getData()
 
-dev_data_T = dev_data.T
-Y_dev = dev_data_T[0]
-X_dev = dev_data_T[1:n]
-X_dev = X_dev / 255.
+# creare obiect tip retea
+    n = retea(m)
 
-train_data_T = train_data.T
-Y_train = train_data_T[0]
+# ajustare greutati si bias-uri
+    n.gradient_descent(X_train, Y_train, 0.15, 500)
 
-X_train = train_data_T[1:n]
-X_train = X_train / 255.
-_,m_train = X_train.shape
+    verifData(n, X_dev, Y_dev)
 
 
-n =  retea()
-n.m = m
+def getData():
+    train_data = pd.read_csv('C:/Users/bogda/PycharmProjects/date/mnist_train.csv')
+    test_data = pd.read_csv('C:/Users/bogda/PycharmProjects/date/mnist_test.csv')
+
+    train_data = np.array(train_data)
+    dev_data = np.array(test_data)
+    m, n = train_data.shape
+
+    dev_data_T = dev_data.T
+    Y_dev = dev_data_T[0]
+    X_dev = dev_data_T[1:n]
+    X_dev = X_dev / 255.
+
+    train_data_T = train_data.T
+    Y_train = train_data_T[0]
+
+    X_train = train_data_T[1:n]
+    X_train = X_train / 255.
+    _,m_train = X_train.shape
+    return m, X_train, Y_train, X_dev, Y_dev
 
 
-W1, b1, W2, b2 = n.gradient_descent(X_train, Y_train, 0.15, 500)
+def verifData(n, X_dev, Y_dev):
+    # verificare date obtinute cu ajutorul datelor de test
 
-n.verif_pred_test(W1, b1, W2, b2, 1000, X_dev, Y_dev)
+    n.verif_pred_test(500, X_dev, Y_dev)
+    t, _ = X_dev.shape
+    j = random.randint(0, t)
+    n.test_prediction(j, X_dev, Y_dev)
+    j = random.randint(0, t)
+    n.test_prediction(j, X_dev, Y_dev)
+    j = random.randint(0, t)
+    n.test_prediction(j, X_dev, Y_dev)
+    j = random.randint(0, t)
+    n.test_prediction(j, X_dev, Y_dev)
 
-j = random.randint(0, t)
-n.test_prediction(j, W1, b1, W2, b2, X_dev, Y_dev)
-j = random.randint(0, t)
-n.test_prediction(j, W1, b1, W2, b2, X_dev, Y_dev)
-j = random.randint(0, t)
-n.test_prediction(j, W1, b1, W2, b2, X_dev, Y_dev)
-j = random.randint(0, t)
-n.test_prediction(j, W1, b1, W2, b2, X_dev, Y_dev)
-
-
+main()
--- neuron.py
+++ neuron.py
@@ -6,9 +6,9 @@ from matplotlib import pyplot as plt
 
 class retea:
 
-    def retea(self, m):
+    def __init__(self, m):
         self.m = m
-
+        self.W1, self.b1, self.W2, self.b2 = self.init_params()
 
     def init_params(self):
         W1 = np.random.rand(10, 784) - 0.5
@@ -27,10 +27,10 @@ class retea:
         return A
 
 
-    def forward_prop(self, W1, b1, W2, b2, X):
-        Z1 = W1.dot(X) + b1
+    def forward_prop(self, X):
+        Z1 = self.W1.dot(X) + self.b1
         A1 = self.ReLU(Z1)
-        Z2 = W2.dot(A1) + b2
+        Z2 = self.W2.dot(A1) + self.b2
         A2 = self.softmax(Z2)
         return Z1, A1, Z2, A2
 
@@ -46,25 +46,25 @@ class retea:
         return one_hot_Y
 
 
-    def backward_prop(self, Z1, A1, Z2, A2, W1, W2, X, Y):
+    def backward_prop(self, Z1, A1, Z2, A2, X, Y):
         one_hot_Y = self.one_hot(Y)
         dZ2 = A2 - one_hot_Y
         dW2 = 1 / self.m * dZ2.dot(A1.T)
         db2 = 1 / self.m * np.sum(dZ2)
 
-        dZ1 = W2.T.dot(dZ2) * self.ReLU_deriv(Z1)
+        dZ1 = self.W2.T.dot(dZ2) * self.ReLU_deriv(Z1)
         dW1 = 1 / self.m * dZ1.dot(X.T)
         db1 = 1 / self.m * np.sum(dZ1)
 
         return dW1, db1, dW2, db2
 
 
-    def update_params(self, W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
-        W1 = W1 - alpha * dW1
-        b1 = b1 - alpha * db1
-        W2 = W2 - alpha * dW2
-        b2 = b2 - alpha * db2
-        return W1, b1, W2, b2
+    def update_params(self, dW1, db1, dW2, db2, alpha):
+        self.W1 = self.W1 - alpha * dW1
+        self.b1 = self.b1 - alpha * db1
+        self.W2 = self.W2 - alpha * dW2
+        self.b2 = self.b2 - alpha * db2
+
 
 
 
@@ -79,28 +79,28 @@ class retea:
 
 
     def gradient_descent(self, X, Y, alpha, iterations):
-        W1, b1, W2, b2 = self.init_params()
+
         for i in range(iterations):
-            Z1, A1, Z2, A2 = self.forward_prop(W1, b1, W2, b2, X)
-            dW1, db1, dW2, db2 = self.backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y)
-            W1, b1, W2, b2 = self.update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha)
+            Z1, A1, Z2, A2 = self.forward_prop(X)
+            dW1, db1, dW2, db2 = self.backward_prop(Z1, A1, Z2, A2, X, Y)
+            self.update_params( dW1, db1, dW2, db2, alpha)
             if i % 100 == 0:
                 print("Iteration: ", i)
                 predictions = self.get_predictions(A2)
                 print("Accuracy = " + str(self.get_accuracy(predictions, Y)))
     #            alpha = alpha - (get_accuracy(predictions, Y) / 500)
     #            print('alpha = ' + str(alpha))
-        return W1, b1, W2, b2
 
-    def make_predictions(self, X, W1, b1, W2, b2):
-        _, _, _, A2 = self.forward_prop(W1, b1, W2, b2, X)
+
+    def make_predictions(self, X):
+        _, _, _, A2 = self.forward_prop(X)
         predictions = self.get_predictions(A2)
         return predictions
 
-    def test_prediction(self, index, W1, b1, W2, b2, X_dev, Y_dev):
+    def test_prediction(self, index, X_dev, Y_dev):
         print("test prediction")
         current_image = X_dev[:, index, None]
-        prediction = self.make_predictions(X_dev[:, index, None], W1, b1, W2, b2)
+        prediction = self.make_predictions(X_dev[:, index, None])
         label = Y_dev[index]
         print("Prediction: ", prediction)
         print("Label: ", label)
@@ -110,16 +110,16 @@ class retea:
         plt.imshow(current_image, interpolation='nearest')
         plt.show()
 
-    def verif_pred_test(self, W1, b1, W2, b2, index, X_dev, Y_dev):
+    def verif_pred_test(self, index, X_dev, Y_dev):
         k = 0
         o = 0
         for i in range(index):
-            _, _, _, A2 = self.forward_prop(W1, b1, W2, b2, X_dev[:, i, None])
+            _, _, _, A2 = self.forward_prop( X_dev[:, i, None])
             test = self.get_predictions(A2)
             if test == Y_dev[i]:
                 k = k + 1
             elif o < 3:
-                self.test_prediction(i, W1, b1, W2, b2, X_dev, Y_dev)
+                self.test_prediction(i, X_dev, Y_dev)
                 o = o + 1
         print("Acuratete teste dev = " + str(k / index))
 
